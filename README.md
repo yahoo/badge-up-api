179f306f54c1ece12ebb535818a7ff8727e9088d
St. John Johnson <st.john.johnson@gmail.com>
2016-04-27 11:47:44 -0700

    Making some major improvements
     - Removing `/api/`
     - Adding documentation to README
     - Supporting hex colors like #FFF or #F0F0F0
     - Adding `npm start` support
     - Fixing package.json

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# badge-up-api
+# Badge Up API
 
-A pretty cool SVG badge serving system.
+A simple API for generating SVG badges without the need of Cairo.
+
+## Usage
+
+```bash
+$ npm install
+$ npm start
+```
--- index.js
+++ index.js
@@ -3,4 +3,10 @@ Copyright (c) 2016, Yahoo Inc.
 Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
 */
 
-require('./lib/server')();
+require('./lib/server')({}, function (error, server) {
+    if (error) {
+        console.error('Failure starting application', error);
+        process.exit(1);
+    }
+    console.log('Running at http://127.0.0.1:' + server.address().port);
+});
